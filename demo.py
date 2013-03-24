24cb9aa41aebcf90dd231f4cb893f4832849ac87
Alex Bennée <alex@bennee.com>
2013-03-24 12:58:01 +0000

    Hack it to insert some text when Foo clicked.

--- demo.py
+++ demo.py
@@ -7,8 +7,21 @@ from gi.repository import Gtk
 
 class Demo(object):
 
+    def registerTextWidgets(self, te1, tv1, tv2):
+        # bring objects into class
+        self.textEntry1 = te1
+        self.textView1 = tv1
+        self.textView2 = tv2
+
     def onDeleteWindow(self, *args):
+        print "delete window"
         Gtk.main_quit(*args)
+
+    def on_fooButton_clicked(self, button):
+        # copy text from input and append
+        buf = self.textView1.get_buffer()
+        buf.insert_at_cursor(self.textEntry1.get_text())
+        
 
     def on_quitButton_clicked(self, button):
         print "quit clicked"
@@ -16,9 +29,15 @@ class Demo(object):
 
 builder = Gtk.Builder()
 builder.add_from_file("demo.glade")
-builder.connect_signals(Demo())
+demo = Demo()
+builder.connect_signals(demo)
 
 window = builder.get_object("window1")
 window.show_all()
+
+# regiser the text objects to make it easier for the class to do stuff
+demo.registerTextWidgets(builder.get_object("textEntry1"),
+                         builder.get_object("textView1"),
+                         builder.get_object("textView2"))
 
 Gtk.main()
